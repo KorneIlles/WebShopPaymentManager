22bad339387a41f8c25527f58e7620167c2ada5d
Illes Kornel <kornelilles10@egmail.com>
2023-05-23 20:40:29 +0200

    create writeClientPaymentSumReport, writeTopCustomer, writeWebShopTotalRevenue methods

--- src/main/java/com/kornelIlles/report/CsvReader.java
+++ src/main/java/com/kornelIlles/report/CsvReader.java
@@ -1,5 +1,6 @@
-package com.kornelIlles.CSVReader;
+package com.kornelIlles.report;
 
+import com.kornelIlles.dto.CustomerPaymentReportDTO;
 import com.kornelIlles.model.customer.Customer;
 import com.kornelIlles.model.payment.CardPayment;
 import com.kornelIlles.model.payment.Payment;
@@ -19,11 +20,13 @@ public class CsvReader {
 
     private final String paymentPath;
     private final String customerPath;
+    private final String customerPaymentSum;
     private static CsvReader csvReader;
 
     private CsvReader() {
         this.paymentPath = Utils.getCsvPath("csv.payments");
         this.customerPath = Utils.getCsvPath("csv.customer");
+        this.customerPaymentSum = Utils.getCsvPath("csv.customerPaymentSum");
     }
 
     public static CsvReader getInstance() {
@@ -41,7 +44,7 @@ public class CsvReader {
             while ((nextLine = reader.readNext()) != null) {
                 Customer customer = convertToCustomer(nextLine[0].split(";"));
                 String key = customer.getWebShopId() + "-" + customer.getClientId();
-                customers.put(key,customer);
+                customers.put(key, customer);
             }
         } catch (CsvValidationException | IOException e) {
             e.printStackTrace();
@@ -58,7 +61,7 @@ public class CsvReader {
             LineValidator myLineValidator = new MyLineValidator();
             while ((nextLine = reader.readNext()) != null) {
                 String[] lineFields = nextLine[0].split(";");
-                if (myLineValidator.isValid(lineFields)){
+                if (myLineValidator.isValid(lineFields)) {
                     Payment payment = convertToPayment(lineFields);
                     payments.add(payment);
                 }
@@ -66,9 +69,30 @@ public class CsvReader {
         } catch (CsvValidationException | IOException e) {
             e.printStackTrace();
         }
-
-
         return payments;
+    }
+
+    public List<CustomerPaymentReportDTO> readCustomerPaymentSum() {
+        List<CustomerPaymentReportDTO> customersPayment = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(customerPaymentSum))) {
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                CustomerPaymentReportDTO customerPaymentReportDTO = convertToCustomerPaymentReportDTO(nextLine[0].split(";"));
+                customersPayment.add(customerPaymentReportDTO);
+
+            }
+        } catch (CsvValidationException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return customersPayment;
+    }
+
+    private CustomerPaymentReportDTO convertToCustomerPaymentReportDTO(String[] customerPaymentData) {
+        String customerName = customerPaymentData[0];
+        String customerAddress = customerPaymentData[1];
+        BigDecimal sumOfPayments = new BigDecimal(customerPaymentData[2]);
+        return new CustomerPaymentReportDTO(customerName, customerAddress, sumOfPayments);
     }
 
     private Payment convertToPayment(String[] PaymentData) {
@@ -94,6 +118,6 @@ public class CsvReader {
         String clientId = customerData[1];
         String clientName = customerData[2];
         String clientAddress = customerData[3];
-        return new Customer(webShopId,clientId,clientName,clientAddress);
+        return new Customer(webShopId, clientId, clientName, clientAddress);
     }
 }
---
+++ src/main/java/com/kornelIlles/report/CsvWriter.java
@@ -0,0 +1,108 @@
+package com.kornelIlles.report;
+
+import com.kornelIlles.dto.CustomerPaymentReportDTO;
+import com.kornelIlles.model.customer.Customer;
+import com.kornelIlles.model.payment.CardPayment;
+import com.kornelIlles.model.payment.Payment;
+import com.kornelIlles.model.payment.TransferPayment;
+import com.kornelIlles.utils.Utils;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class CsvWriter {
+
+    private static CsvWriter csvWriter;
+    private final String customerPaymentSum;
+    private final String topCustomerPayment;
+    private final String webShopTotalRevenue;
+
+
+    private CsvWriter() {
+        this.customerPaymentSum = Utils.getCsvPath("csv.customerPaymentSum");
+        this.topCustomerPayment = Utils.getCsvPath("csv.topCustomerPayment");
+        this.webShopTotalRevenue = Utils.getCsvPath("csv.webShopTotalRevenue");
+    }
+
+    public static CsvWriter getInstance() {
+        if (csvWriter == null) {
+            csvWriter = new CsvWriter();
+        }
+        return csvWriter;
+    }
+
+    public void writeClientPaymentSumReport(Map<Customer, List<Payment>> customerPayments) {
+        try (PrintWriter pw = new PrintWriter(customerPaymentSum)) {
+            for (Map.Entry<Customer, List<Payment>> entry : customerPayments.entrySet()) {
+                Customer customer = entry.getKey();
+                List<Payment> payments = entry.getValue();
+                String line = String.format("%s;%s;%.2f", customer.getClientName(), customer.getClientAddress(), sumPayment(payments));
+                pw.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeTopCustomer(List<CustomerPaymentReportDTO> topCustomers) {
+        try (PrintWriter pw = new PrintWriter(topCustomerPayment)) {
+            for (CustomerPaymentReportDTO topCustomer : topCustomers) {
+                String line = String.format("%s;%s;%.2f", topCustomer.getCustomerName(), topCustomer.getCustomerAddress(), topCustomer.getSumOfPayment());
+                pw.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeWebShopTotalRevenue(List<Payment> payments) {
+        Map<String, BigDecimal[]> webShopTotals = getWebShopIdsAndRevenues(payments);
+
+        try (PrintWriter pw = new PrintWriter(webShopTotalRevenue)) {
+            for (Map.Entry<String, BigDecimal[]> entry : webShopTotals.entrySet()) {
+                String webShopId = entry.getKey();
+                BigDecimal[] totals = entry.getValue();
+                BigDecimal transactionTotal = totals[0];
+                BigDecimal cardPurchaseTotal = totals[1];
+
+                String line = String.format("%s;%.2f;%.2f", webShopId, cardPurchaseTotal, transactionTotal);
+                pw.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static Map<String, BigDecimal[]> getWebShopIdsAndRevenues(List<Payment> payments) {
+        Map<String, BigDecimal[]> webShopTotals = new HashMap<>();
+
+        for (Payment payment : payments) {
+            String webShopId = payment.getWebShopId();
+            BigDecimal paymentAmount = payment.getAmount();
+
+            BigDecimal[] currentTotals = webShopTotals.getOrDefault(webShopId, new BigDecimal[]{BigDecimal.ZERO, BigDecimal.ZERO});
+
+            if (payment instanceof TransferPayment && ((TransferPayment) payment).getPaymentType().equalsIgnoreCase("transfer")) {
+                BigDecimal transactionTotal = currentTotals[0].add(paymentAmount);
+                currentTotals[0] = transactionTotal;
+            } else if (((CardPayment) payment).getPaymentType().equalsIgnoreCase("card")) {
+                BigDecimal cardPurchaseTotal = currentTotals[1].add(paymentAmount);
+                currentTotals[1] = cardPurchaseTotal;
+            }
+
+            webShopTotals.put(webShopId, currentTotals);
+        }
+        return webShopTotals;
+    }
+
+    private static BigDecimal sumPayment(List<Payment> payments) {
+        return payments.stream().map(Payment::getAmount).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+}
